490514a57c44ffef28944c1a95141c6e34d6097e
Keisuke Toyota <hm.pudding0715@gmail.com>
2026-09-05 20:28:24 +0900

    chore: update workspace dependencies

--- crates/toyoterm-cli/build.rs
+++ crates/toyoterm-cli/build.rs
@@ -1,5 +1,7 @@
 fn main() {
     println!("cargo:rerun-if-changed=../../packaging/windows/toyoterm.rc");
     println!("cargo:rerun-if-changed=../../packaging/app-icon.ico");
-    embed_resource::compile("../../packaging/windows/toyoterm.rc", embed_resource::NONE);
+    embed_resource::compile("../../packaging/windows/toyoterm.rc", embed_resource::NONE)
+        .manifest_optional()
+        .unwrap();
 }
